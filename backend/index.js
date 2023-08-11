654185791cea6115fd9f58aee2687687001321a3
bilal-ahmed89 <ba1770490@gmail.com>
2023-08-11 17:44:55 +0500

    modifive

--- backend/index.js
+++ backend/index.js
@@ -32,9 +32,10 @@ const corsOptions ={
     origin:'*', 
     credentials:true          
  }
+ app.use(express.static(path.join(__dirname, 'build')));
   app.get("/*", function(req,res){
     res.sendFile(
-        path.join(__dirname, '../frontend/build/index.html'),
+        path.join(__dirname, 'build', 'index.html'),
         function(err){
             if(err){
                 res.status(500).send(err)
